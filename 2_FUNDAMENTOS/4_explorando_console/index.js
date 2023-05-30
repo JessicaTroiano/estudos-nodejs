5f7b30c99b850abec37c07fcd05920fa644c9c04
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-29 22:18:40 -0300

    feat: add modulo explorando o console

---
+++ 2_FUNDAMENTOS/4_explorando_console/index.js
@@ -0,0 +1,16 @@
+const x = 10;
+const y = 'meu texto';
+const z = [3,2,1];
+
+console.log(x, y, z);
+
+console.count(x);
+console.count(x);
+
+//variável entre string
+console.log("O nome é %s", y);
+
+//limpar o console
+setTimeout(() => {
+    console.clear()
+}, 2000)
