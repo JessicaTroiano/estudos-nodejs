7793a1289b1d706cf7e67aad350315cd40fb38ea
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-30 22:24:46 -0300

    feat: add modulo anstracao input

--- 2_FUNDAMENTOS/6_lendo-input/index.js
+++ 2_FUNDAMENTOS/6_lendo-input/index.js
@@ -2,7 +2,7 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 })
-
+//implementa interação de input com o usuário
 readline.question('Qual a sua linguagem preferida? ', (language) => {
     console.log(`A minha linguagem preferida é: ${language}`)
     readline.close();
---
+++ 2_FUNDAMENTOS/7_abstracao_input/index.mjs
@@ -0,0 +1,14 @@
+import inquirer from 'inquirer';
+
+inquirer.prompt([
+  {
+    name: 'p1',
+    message: 'Qual a primeira nota?'
+  },
+  {
+    name: 'p2',
+    message: 'Qual a segunda nota?'
+  }
+]).then((answer) => {
+  console.log(answer)
+}).catch(err => console.log(err));
