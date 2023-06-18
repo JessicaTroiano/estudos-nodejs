031adbe409ff909ad36deffccd3628b7e8f06026
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-06-18 19:14:26 -0300

    feat: add secao colocando css

---
+++ 6_EXPRESS/9_colocando_css/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const port = 3000;
+
+const usersRoutes = require('./users');
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(express.json());
+
+//com esse middleware é possível adicionar css ao projeto com express.
+app.use(express.static('public'))
+
+//__dirname -> meu diretório atual, no caso seria o render_html
+const basePath = path.join(__dirname, 'templates');
+
+app.use('/users', usersRoutes)
+
+app.get('/', (req, res) => {
+    //acessa pasta/arquivo desejado
+    res.sendFile(`${basePath}/index.html`)
+});
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
+
